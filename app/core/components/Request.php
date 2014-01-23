69ce04fc00c9d860da2c6f0cee4978bf2e071710
benoit ciret <benoit.ciret@supinternet.fr>
2014-01-23 19:05:00 +0100

    clean test, var_dump, echo

--- app/core/components/Request.php
+++ app/core/components/Request.php
@@ -5,15 +5,17 @@ namespace core\components;
 class Request
 {
   private $properties;
+  private $headers;
 
   public function __construct()
   {
+    var_dump($_SERVER);
     $env = array();
 
-    $env['REQUEST_METHOD'] = $_SERVER['REQUEST_METHOD'];
-    $env['REQUEST_OPTION'] = $this->extract($_SERVER['PHP_SELF'],$_SERVER['SCRIPT_NAME']);
     $env['SERVER_PORT'] = $_SERVER['SERVER_PORT'];
     $env['SERVER_NAME'] = $_SERVER['SERVER_NAME'];
+    $env['REQUEST_METHOD'] = $_SERVER['REQUEST_METHOD'];
+    $env['REQUEST_OPTION'] = $this->extract($_SERVER['PHP_SELF'],$_SERVER['SCRIPT_NAME']);
 
     $this->properties = $env;
   }
