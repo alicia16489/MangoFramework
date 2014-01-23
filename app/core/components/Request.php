91d16e9fd275aac26828df4e30b2ea562d7624f2
benoit ciret <benoit.ciret@supinternet.fr>
2014-01-23 09:23:29 +0100

    fix container loaders components

--- app/core/components/Request.php
+++ app/core/components/Request.php
@@ -4,12 +4,8 @@ namespace core\components;
 
 class Request
 {
-  private $request_method;
-  private $request_uri;
 
   public function __construct()
   {
-    $this->request_method = $_SERVER['REQUEST_METHOD'];
-    var_dump($_SERVER);
   }
 }
