ac71c18b9fdb243d12fe6e2c329776f28eb6de53
benoit ciret <benoit.ciret@supinternet.fr>
2014-01-23 09:29:00 +0100

    merge for rebase request to app

--- app/core/components/Request.php
+++ app/core/components/Request.php
@@ -4,8 +4,18 @@ namespace core\components;
 
 class Request
 {
+<<<<<<< HEAD
 
   public function __construct()
   {
+=======
+  private $request_method;
+  private $request_uri;
+
+  public function __construct()
+  {
+    $this->request_method = $_SERVER['REQUEST_METHOD'];
+    var_dump($_SERVER);
+>>>>>>> proto request class
   }
 }
