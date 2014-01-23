cf1aaa042220ad433872b773f799d2f75de39bd4
benoit ciret <benoit.ciret@supinternet.fr>
2014-01-23 18:35:12 +0100

    request class properties

--- app/core/components/Request.php
+++ app/core/components/Request.php
@@ -4,18 +4,33 @@ namespace core\components;
 
 class Request
 {
-<<<<<<< HEAD
+  private $properties;
+
+  public function getRequestMethod()
+  {
+    return $this->requestMethod;
+  }
+
+  public function getRequestString()
+  {
+    return $this->requestString;
+  }
 
   public function __construct()
   {
-=======
-  private $request_method;
-  private $request_uri;
-
-  public function __construct()
-  {
-    $this->request_method = $_SERVER['REQUEST_METHOD'];
     var_dump($_SERVER);
->>>>>>> proto request class
+    $env = array();
+
+    $env['REQUEST_METHOD'] = $_SERVER['REQUEST_METHOD'];
+    $env['REQUEST_OPTION'] = $this->extract($_SERVER['PHP_SELF'],$_SERVER['SCRIPT_NAME']);
+    $env['SERVER_PORT'] = $_SERVER['SERVER_PORT'];
+    $env['SERVER_NAME'] = $_SERVER['SERVER_NAME'];
+
+    $this->properties = $env;
+  }
+
+  public function extract($phpSelf,$scriptName)
+  {
+    return str_replace($scriptName,'',$phpSelf);
   }
 }
