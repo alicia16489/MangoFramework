2538619e03b9f52008c5b443fbb344287dbaff63
benoit ciret <benoit.ciret@supinternet.fr>
2014-01-24 03:55:29 +0100

    add REQUEST_OPTION_PARTS in env

--- app/core/components/Request.php
+++ app/core/components/Request.php
@@ -4,18 +4,20 @@ namespace core\components;
 
 class Request
 {
-  private $properties;
-  private $headers;
+  public $properties;
+  public $headers;
 
   public function __construct()
   {
-    var_dump($_SERVER);
     $env = array();
 
     $env['SERVER_PORT'] = $_SERVER['SERVER_PORT'];
     $env['SERVER_NAME'] = $_SERVER['SERVER_NAME'];
     $env['REQUEST_METHOD'] = $_SERVER['REQUEST_METHOD'];
     $env['REQUEST_OPTION'] = $this->extract($_SERVER['PHP_SELF'],$_SERVER['SCRIPT_NAME']);
+    $env['REQUEST_OPTION_PARTS'] = explode("/",$env['REQUEST_OPTION']);
+
+    var_dump($env);
 
     $this->properties = $env;
   }
