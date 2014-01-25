fe0242bf578e823aa5d3b4b3a2d336b8a2849b0e
benoit ciret <benoit.ciret@supinternet.fr>
2014-01-25 23:04:29 +0100

    clean

--- app/core/components/Request.php
+++ app/core/components/Request.php
@@ -15,7 +15,6 @@ class Request
     $env['SERVER_NAME'] = $_SERVER['SERVER_NAME'];
     $env['REQUEST_METHOD'] = $_SERVER['REQUEST_METHOD'];
     $env['REQUEST_OPTION'] = (empty($_SERVER['PATH_INFO']))? '/' : $_SERVER['PATH_INFO'];
-    var_dump($env);
     $env['REQUEST_OPTION_PARTS'] = explode("/",$env['REQUEST_OPTION']);
 
     $this->properties = $env;
